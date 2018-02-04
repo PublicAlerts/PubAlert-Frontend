9fa2f0c324ff3150b2cafd7fb617855270db26b7
dalhun namgung <bnamgung@gmail.com>
2018-02-03 17:13:40 -0800

    working on

--- client/src/app/actions/routes.js
+++ client/src/app/actions/routes.js
@@ -1,6 +1,7 @@
-//Route Switcher Action to change the route dynamically
-
 export const switchRoute = (route) => ({
   type: "SWITCH_ROUTE",
   payload: route
 });
+
+
+//Route Switcher Action to change the route dynamically
