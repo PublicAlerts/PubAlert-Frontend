667b39206564084e16e6e909c24dcab4cafa2f8a
Maks <pvl1720@gmail.com>
2018-02-05 22:33:25 -0800

    basic Submit button which can console log what I type now

--- src/alerts-in.js
+++ src/alerts-in.js
@@ -3,15 +3,34 @@ import React, {Component} from 'react';
 class AlertsIn extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      name: ''
+    }
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
   }
+  handleNameChange(e){
+    this.setState({
+      name: e.target.value
+    })
+    // this.state.name = e.target.value
+  }
+
+  handleSubmit(e){
+    console.log(this.state.name);
+    e.preventDefault()
+  }
+
   render() {
     return (
       <div>
         <h1>Alert In</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label>
             Name:
-            <input type="text" name="name" />
+            <input type="text" name="name" value={this.state.name} onChange={this.handleNameChange}/>
           </label>
           <input type="submit" value="Submit" />
         </form>
