d231075636272a20ee95a9cb7529c2f6453fee0a
Maks <pvl1720@gmail.com>
2018-02-06 19:58:58 -0800

    displaying alerts in OK format

--- src/alerts-in.js
+++ src/alerts-in.js
@@ -7,16 +7,20 @@ class AlertsIn extends React.Component {
     this.state = {
       name: '',
       eventLocation: 'downtown',
-      eventInfo: ''
+      eventInfo: '',
+      eventName: '',
+      eventType: 'other'
     }
-
-    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleNameInput = this.handleNameInput.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.handleEnterEventName = this.handleEnterEventName.bind(this);
     this.handleEnterEventInfo = this.handleEnterEventInfo.bind(this);
+    this.handleEnterEventType = this.handleEnterEventType.bind(this);
+
   }
 
-  handleNameChange(e){
+  handleNameInput(e){
     this.setState({
       name: e.target.value
     })
@@ -35,22 +39,38 @@ class AlertsIn extends React.Component {
     })
   }
 
+  handleEnterEventName(e){
+    this.setState({
+      eventName: e.target.value
+    })
+  }
+
+  handleEnterEventType(e){
+    this.setState({
+      eventType: e.target.value
+    })
+  }
+
   handleSubmit(e){
-    //use superagent to POST data to server
+    //using superagent to POST data to server
     e.preventDefault();
     superagent.post('https://pass-backend.herokuapp.com/api/alerts')
     .send({
       userid: this.state.name,
+      eventName: this.state.eventName,
       eventInfo: this.state.eventInfo,
-      eventName: "eventNameJazz",
       eventLocation: this.state.eventLocation,
     })
-    .then(data => console.log(data))
+    .then(data => {
+      this.props.onPostComplete(data.body);
+    })
     .catch(err =>  console.log(err));
 
     console.log(this.state.name);
     console.log(this.state.eventLocation);
     console.log(this.state.eventInfo);
+    console.log(this.state.handleEnterEventType);
+    console.log(this.state.eventName);
     this.setState({
       name: ''
     })
@@ -59,11 +79,11 @@ class AlertsIn extends React.Component {
   render() {
     return (
       <div>
-        <h1>Alert In</h1>
+        <h1>Alerts In</h1>
         <form onSubmit={this.handleSubmit}>
 
-            Name:
-            <input type="text" name="name" placeholder= ' User Name' value={this.state.name} onChange={this.handleNameChange}/>
+            Your Name:
+            <input type="text" name="name" placeholder = ' your name' value={this.state.name} onChange={this.handleNameInput}/>
 
             <div>
               City:
@@ -75,13 +95,38 @@ class AlertsIn extends React.Component {
                 <option value="downtown">Downtown Seattle</option>
                 <option value="tacoma">Tacoma</option>
                 <option value="kent">Kent</option>
+                <option value="uw">U-District</option>
+                <option value="seatac">SeaTac</option>
+                <option value="edmonds">Edmonds</option>
+                <option value="everett">Everett</option>
+                <option value="issaquah">Issaquah</option>
+              </select>
+            </div>
+
+            <div>
+            Event Name:
+            <input type='text' name='name' placeholder = ' name of event' value={this.state.eventName} onChange={this.handleEnterEventName}/>
+            </div>
+
+            <div>
+              Event Type:
+              <select value={this.state.eventType} onChange={this.handleEnterEventType}>
+                <option value="amber">AMBER ALERT</option>
+                <option value="danger">DANGER</option>
+                <option value="traffic">Traffic</option>
+                <option value="social">Social Gathering</option>
+                <option value="food">Food</option>
+                <option value="show">Performance</option>
+                <option value="class">Classes</option>
+                <option value="other">Other</option>
               </select>
             </div>
 
             <div>
               Event Details:
-              <input type='text' info='info' placeholder= ' Event Description' value={this.state.eventInfo} onChange={this.handleEnterEventInfo}/>
+              <input type='text' info='info' placeholder= ' event description' value={this.state.eventInfo} onChange={this.handleEnterEventInfo}/>
             </div>
+
 
           <input type="submit" value="Submit" />
         </form>
--- src/main.js
+++ src/main.js
@@ -1,22 +1,50 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom'
+import superagent from 'superagent';
 import './style/main.scss';
 import AlertsIn from './alerts-in.js';
 import AlertsOut from './alerts-out.js';
-// import {Form, Field} from 'simple-react-forms';
-
 
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      alerts: []
+    }
+    //updates state with another GET req to show new alerts with the rest
+    this.handlePostComplete = this.handlePostComplete.bind(this);
   }
 
+  componentDidMount() {
+    // GET
+    superagent.get('https://pass-backend.herokuapp.com/api/alerts')
+    .then(res =>
+      this.setState({
+        alerts: res.body
+      }))
+    .catch(function(err){
+      console.log(err);
+    })
+  }
+
+  //populating alerts, again, to show with newest one on the page
+  handlePostComplete(data) {
+    console.log('in main', data);
+    superagent.get('https://pass-backend.herokuapp.com/api/alerts')
+    .then(res =>
+      this.setState({
+      alerts: res.body
+      }))
+    .catch(function(err){
+      console.log(err);
+    })
+  }
     render() {
         return (
           <div>
-            <AlertsIn/>
-            <AlertsOut/>
+            <AlertsIn onPostComplete={this.handlePostComplete}/>
+            <AlertsOut alerts={this.state.alerts}/>
           </div>
         )
     }
