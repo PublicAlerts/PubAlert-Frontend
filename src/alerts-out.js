614aa409b7a4f426aba89bbee6b33c136f0a7d0b
Maks <pvl1720@gmail.com>
2018-02-06 20:25:40 -0800

    spacing between alerts as a paragraph

--- src/alerts-out.js
+++ src/alerts-out.js
@@ -12,7 +12,7 @@ class AlertsOut extends React.Component {
             <h1>Alerts</h1>
             <ul>
               {this.props.alerts.map(alert =>
-                <li key={alert._id}> {alert.eventName}! <br/> {alert.eventType} {alert.eventInfo}. <br/> Location: {alert.eventLocation}. <br/>Reported by {alert.userid} {moment(alert.entryDate).fromNow()}.</li>
+                <li key={alert._id}> {alert.eventName}! <br/> {alert.eventType} {alert.eventInfo}. <br/> Location: {alert.eventLocation}. <br/>Reported by {alert.userid} {moment(alert.entryDate).fromNow()}. <p/> </li>
               )}
             </ul>
 
