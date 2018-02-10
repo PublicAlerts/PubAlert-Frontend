1a51c10f05bd87c586368cf8141f80675379e4a9
Maks <pvl1720@gmail.com>
2018-02-09 20:31:37 -0800

    WORKING and not showing hidden alerts when buttons are pressed

--- src/alerts-out.js
+++ src/alerts-out.js
@@ -7,29 +7,34 @@ class AlertsOut extends React.Component {
   }
 
   render() {
+
+    let filteredAlerts = JSON.parse(localStorage.getItem('filteredAlerts')) || []
+
     return (
       <div>
       <h1>Alerts</h1>
       <ul>
 
       {this.props.alerts.map(alert =>
-        <li key={alert._id}>
-        Event: {alert.eventName} <br/>
-        Type: {alert.eventType} <br/>
-        Info: {alert.eventInfo}. <br/>
-        Location: {alert.eventLocation}. <br/>
-        Alert Votes: {alert.alertVotes} <br/>
 
-        Verify this Alert:
-        <button id='voteTrue' onClick={() => this.props.handleVote(alert, 1)}>  </button>
-        <button id='voteFalse' onClick={() => this.props.handleVote(alert, -1)}> </button>
-        <br/>
-        Reported by "{alert.userid}" {moment(alert.entryDate).fromNow()}.<br/>
-        <button id='deleteAlert' className={this.deleteButton} onClick={() => this.props.handleHide(alert)}>
-        Hide Alert
-        </button><br/>
-        <p/>
-        </li>
+            <li key={alert._id}>
+            Event: {alert.eventName} <br/>
+            Type: {alert.eventType} <br/>
+            Info: {alert.eventInfo}. <br/>
+            Location: {alert.eventLocation}. <br/>
+            Alert Votes: {alert.alertVotes} <br/>
+
+            Verify this Alert:
+            <button id='voteTrue' onClick={() => this.props.handleVote(alert, 1)}>  </button>
+            <button id='voteFalse' onClick={() => this.props.handleVote(alert, -1)}> </button>
+            <br/>
+            Reported by "{alert.userid}" {moment(alert.entryDate).fromNow()}.<br/>
+            <button id='deleteAlert' className={this.deleteButton} onClick={() => this.props.handleHide(alert)}>
+            Hide Alert
+            </button><br/>
+            <p/>
+            </li>
+        
       )}
 
 
