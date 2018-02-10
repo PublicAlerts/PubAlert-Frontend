8aaffa984a686d5c82d0eaa222bcc2716ecbf4f1
Maks <pvl1720@gmail.com>
2018-02-10 13:02:14 -0800

    buttons sort

--- src/alerts-out.js
+++ src/alerts-out.js
@@ -32,8 +32,8 @@ class AlertsOut extends React.Component {
         <h1>Alerts</h1>
 
         Sort Alerts<br/>
-        <button id='sortByTime' title='Time' onClick={() => this.setState({sortByTime:true})}> Sort by time </button>
-        <button id='sortByVotes' title='Ranking' onClick={() => {this.setState({sortByTime:false}); console.log(this.state)}}> Sort by Votes </button>
+        <button id='sortByTime' title='Sort by time' onClick={() => this.setState({sortByTime:true})}> </button>
+        <button id='sortByVotes' title='Sort by Votes' onClick={() => {this.setState({sortByTime:false}); console.log(this.state)}}>  </button>
 
       <ul>
       {sortedAlerts.map(alert =>
